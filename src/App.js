49821fd66d42f00af5f089e248cadd5d698145dd
Ankit Vashisht <ankitvashisht12@gmail.com>
2021-03-21 12:30:42 +0530

    Initial setup
    1. Added semantic ui and react query lib.
    2. Added basic homepage
    3. Deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<>
+    <h1>The Cat APP 🐱 : Million Dollar Startup 🤑</h1>
+    <input type='text' placeholder='Search Here by Breed'/>
+    <div>
+      <div>
+        List of Breeds
+      </div>
+      <div>
+        Images of CATS
+      </div>
     </div>
-  );
+  </>);
 }
 
 export default App;
